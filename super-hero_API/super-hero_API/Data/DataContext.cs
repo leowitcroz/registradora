3b1300b0f7c7cda3119c79d40ec116464cd23cfd
Leonardo <leowitcroz@gmail.com>
2022-10-08 19:14:47 -0300

    first commit

---
+++ super-hero_API/super-hero_API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace super_hero_API.Data
+{
+    public class DataContext : DbContext
+    {
+      public DataContext(DbContextOptions<DataContext>options): base(options){ }
+
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
